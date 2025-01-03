035e1d7d043d76317682e0eab0ed5797ac795c69
ohayo66666 <ohayo66666@gmail.com>
2025-01-03 12:27:36 +0000

    new file:   airtable.browser.js 	new file:   app.js 	new file:   css.css 	new file:   index.html 	new file:   output.json

---
+++ app.js
@@ -0,0 +1,21 @@
+// 新しいACCESSトークン
+const apiKey = 'patwjHZtEqvMsKIa5'; 
+const baseId = 'appwZI1fI2TvlsnFA';
+const tableName = 'tblx8EMhrtRnVyEfh';
+
+// Airtableのインスタンス作成
+const airtable = Airtable.configure({ apiKey: apiKey });
+const base = airtable.base(baseId);
+
+// データを取得する処理
+base(tableName).select({
+  maxRecords: 3,
+  view: 'Grid view'
+}).eachPage(function page(records, fetchNextPage) {
+  records.forEach(function(record) {
+    console.log(record.get('Field Name'));
+  });
+  fetchNextPage();
+}, function done(err) {
+  if (err) { console.error(err); return; }
+});  // ここで閉じ括弧を追加
