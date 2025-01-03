1d984529d260d1b878cc5cdb1da34a5be777268f
ohayo66666 <136098013+ohayo66666@users.noreply.github.com>
2025-01-03 21:50:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,18 +6,20 @@ const baseId = 'appwZI1fI2TvlsnFA';
 const tableName = 'tblx8EMhrtRnVyEfh';
 
 // Airtableのインスタンス作成
-const airtable = Airtable.configure({ apiKey: apiKey });
+const airtable = new Airtable({ apiKey });
 const base = airtable.base(baseId);
 
-// データを取得する処理
 base(tableName).select({
-  maxRecords: 3,
-  view: 'Grid view'
+    maxRecords: 3,
+    view: 'Grid view'
 }).eachPage(function page(records, fetchNextPage) {
-  records.forEach(function(record) {
-    console.log(record.get('Field Name'));
-  });
-  fetchNextPage();
+    records.forEach(record => {
+        console.log(record.fields);
+    });
+    fetchNextPage();
 }, function done(err) {
-  if (err) { console.error(err); return; }
-});  // ここで閉じ括弧を追加
+    if (err) {
+        console.error(err);
+        return;
+    }
+});
